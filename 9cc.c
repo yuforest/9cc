001383d5a6d447282b4a19d453da5e0292a40748
大森優太 <>
2023-03-05 15:43:39 +0900

    feat: 足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,9 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov x0, %d\n", atoi(argv[1]));
+  printf("  mov x0, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add x0, x0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub x0, x0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
+  return 0;
   return 0;
 }
