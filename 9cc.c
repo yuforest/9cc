3425d1033ac2464d43a5fe48bbe5db6d55443cfb
大森優太 <>
2023-03-09 22:12:57 +0900

    feat: 複数文字変数に対応

--- 9cc.c
+++ 9cc.c
@@ -9,10 +9,18 @@ int main(int argc, char **argv) {
   // トークナイズする
   user_input = argv[1];
   token = tokenize(user_input);
-  Node *node = program();
+  Program *prog = program();
+
+  // Assign offsets to local variables.
+  int offset = 0;
+  for (Var *var = prog->locals; var; var = var->next) {
+    offset += 8;
+    var->offset = offset;
+  }
+  prog->stack_size = offset;
 
 
   // コード生成
-  codegen(node);
+  codegen(prog);
   return 0;
 }
